b4ef38175c35aeedf4c1a617420b44ec65d0045a
ryu junho <ryujunho@ryus-MacBook-Pro.local>
2022-09-16 19:01:05 +0900

    PRD-4134 보일러플레이트 코드 업데이트

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,24 +1,13 @@
 import { Hydrate, QueryClientProvider } from 'react-query';
-import React, { useEffect } from 'react';
 
 import { Global } from '@emotion/react';
 import { GlobalStyles } from '@/shared/styles/global';
 import { I18nextProvider } from 'react-i18next';
-import colorSet from '@/shared/configs/colorSet.json';
-import cssVars from 'css-vars-ponyfill';
+import React from 'react';
 import i18n from '@/locales/i18n';
 import queryClient from '@/shared/utils/queryClient';
 
 const App = ({ Component, pageProps }): React.ReactElement => {
-  const selectedColor = 'default';
-
-  useEffect(() => {
-    cssVars({
-      rootElement: document.documentElement,
-      variables: { ...colorSet[selectedColor] },
-    });
-  }, []);
-
   return (
     <I18nextProvider i18n={i18n}>
       <QueryClientProvider client={queryClient}>
