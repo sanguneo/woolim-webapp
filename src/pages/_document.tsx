8821ecd8e8684c9b0c3dc2f86c42170348201351
ryu junho <ryujunho@ryus-MacBook-Pro.local>
2022-09-19 16:43:10 +0900

    PRD-4134 페이지 head 위치 개선

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,15 +1,10 @@
-/* eslint-disable @next/next/no-title-in-document-head */
-import { Head, Html, Main, NextScript } from 'next/document';
+import { Html, Main, NextScript } from 'next/document';
 
 import React from 'react';
 
 export default function MyDocument() {
   return (
     <Html>
-      <Head>
-        <title>Boilerplate Next</title>
-        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
-      </Head>
       <body>
         <Main />
         <NextScript />
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { HomeContainer, getExampleState, setExampleState } from '@/shared/constants/pages';
 import React, { useEffect } from 'react';
 
+import Head from 'next/head';
 import type { NextPage } from 'next';
 import Posts from '@/components/Posts/Posts';
 import { dehydrate } from 'react-query';
@@ -20,10 +21,16 @@ const Home: NextPage = (): React.ReactElement => {
   useEffect(() => setExample('Durian NEXT exmaple'), []);
 
   return (
-    <HomeContainer>
-      <h1>{example}</h1>
-      <Posts />
-    </HomeContainer>
+    <>
+      <Head>
+        <title>Boilerplate Next</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <HomeContainer>
+        <h1>{example}</h1>
+        <Posts />
+      </HomeContainer>
+    </>
   );
 };
 
