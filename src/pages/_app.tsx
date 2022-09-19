32ffecb765cf8edcabb272e535891c7ee9bef88c
ryu junho <ryujunho@ryus-MacBook-Pro.local>
2022-09-19 10:26:33 +0900

    PRD-4134 react-query devtools 추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,14 +4,17 @@ import { Global } from '@emotion/react';
 import { GlobalStyles } from '@/shared/styles/global';
 import { I18nextProvider } from 'react-i18next';
 import React from 'react';
+import { ReactQueryDevtools } from 'react-query/devtools';
 import i18n from '@/locales/i18n';
 import queryClient from '@/shared/utils/queryClient';
 
 const App = ({ Component, pageProps }): React.ReactElement => {
+  const env = process.env.NODE_ENV;
   return (
     <I18nextProvider i18n={i18n}>
       <QueryClientProvider client={queryClient}>
         <Global styles={GlobalStyles} />
+        {env === 'development' ? <ReactQueryDevtools initialIsOpen={false} /> : null}
         <Hydrate state={pageProps.dehydratedState}>
           <Component {...pageProps} />
         </Hydrate>
