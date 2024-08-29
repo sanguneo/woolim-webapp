51573bc50ff4903463b7d5f7e3fd8a0cd9e14e93
sanguneo <sknah0319@gmail.com>
2024-08-29 10:22:14 +0900

    Updates

--- _next/static/titKaEFrf_V2pLcjem_44/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/66-5f69e413fab698e0.js","static/css/891bcedf0385e829.css","static/chunks/pages/index-d2c74ddfdb80a564.js"],"/_error":["static/chunks/pages/_error-77823ddac6993d35.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
