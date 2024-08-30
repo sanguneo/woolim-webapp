5012391583f1674f89563d60c26cf0a4afb9046e
sanguneo <sknah0319@gmail.com>
2024-08-30 16:25:19 +0900

    change to 14

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -46,14 +46,12 @@ export default function Home() {
   return (
     <>
       <main className={'h-screen overflow-y-scroll relative flex flex-col items-center'}>
-        <nav
-          className={'w-full h-12 max-w-[500px] min-w-[320px] sticky top-0 gap-4 flex justify-center py-2 bg-white flex-shrink-0 flex-grow-0'}>
-          <button onClick={onClickRefetch} className={'text-xs'}>새로고침 ↺</button>
-          <select value={searchCategory} onChange={onChangeCategory} className={'w-20'}>
+        <nav className={'w-full h-12 max-w-[500px] min-w-[300px] sticky top-0 gap-4 flex justify-center py-2 bg-white flex-shrink-0 flex-grow-0'}>
+          <button onClick={onClickRefetch} className={'text-xs whitespace-pre w-16'}>새로고침 ↺</button>
+          <select value={searchCategory} onChange={onChangeCategory} className={'w-16 whitespace-pre'}>
             {keys.map(key => <option value={key} key={key}>{key}</option>)}
           </select>
-          <input type="text" placeholder={'검색어 입력'} onChange={onChangeQuery} value={query}
-                 className={'flex-[1] h-8 py-0'}/>
+          <input type="text" placeholder={'검색어 입력'} onChange={onChangeQuery} value={query} className={'w-[calc(100%-8rem)] h-8 py-0'}/>
         </nav>
         <ul className={`${listModule.list} ${listModule.head}`}>
           <li>
@@ -71,7 +69,7 @@ export default function Home() {
             <div>{room['명칭']}</div>
           </li>)}
         </ul>
-        {activeRoom && <div className={'h-screen w-full max-w-[500px] min-w-[360px] fixed top-0 bg-white px-4 py-2 flex flex-col justify-center'}>
+        {activeRoom && <div className={'h-screen w-full max-w-[500px] min-w-[300px] fixed top-0 bg-white px-4 py-2 flex flex-col justify-center'}>
           <b className="text-3xl absolute top-2 right-4 cursor-pointer" onClick={()=>setActiveRoom(null)}>&times;</b>
           <ul className={'flex flex-col gap-4'}>
             {keys.map(key => key !== 'idx' && <li key={key} className={'flex w-full gap-4'}>
