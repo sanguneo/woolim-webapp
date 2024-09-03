c006eee94f8a2a8820882a3cf72e0fb88c578ef9
sanguneo <sknah0319@gmail.com>
2024-09-04 08:42:35 +0900

    re install

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -73,9 +73,9 @@ export default function Home() {
 
   return (
     <>
-      <main className={'h-screen overflow-y-scroll relative flex flex-col items-center'}>
-        <nav className={'w-full h-12 max-w-[500px] min-w-[300px] sticky top-0 gap-4 flex justify-center py-2 bg-white flex-shrink-0 flex-grow-0'}>
-          {isInstallable && <button onClick={handleInstallClick} className={'text-xs whitespace-pre w-16 font-bold'}>설치</button>}
+      <main className={'w-full h-screen overflow-y-scroll relative flex flex-col items-center'}>
+        <nav className={'w-full max-w-[640px] min-w-[320px] h-12 sticky top-0 gap-2 flex justify-center py-2 bg-white flex-shrink-0 flex-grow-0'}>
+          {isInstallable && <button onClick={handleInstallClick} className={'text-xs whitespace-pre w-8 font-bold'}>설치</button>}
           <button onClick={onClickRefetch} className={'text-xs whitespace-pre w-16'}>새로고침 ↺</button>
           <select value={searchCategory} onChange={onChangeCategory} className={'w-16 whitespace-pre'}>
             {keys.map(key => key !== 'idx' && <option value={key} key={key}>{key}</option>)}
@@ -98,7 +98,7 @@ export default function Home() {
             <div>{room['명칭']}</div>
           </li>)}
         </ul>
-        {activeRoom && <div className={'h-screen w-full max-w-[500px] min-w-[300px] fixed top-0 bg-white px-4 py-2 flex flex-col justify-center'}>
+        {activeRoom && <div className={'h-screen w-full max-w-[640px] min-w-[320px] fixed top-0 bg-white px-4 py-2 box-border flex flex-col justify-center'}>
           <b className="text-3xl absolute top-2 right-4 cursor-pointer" onClick={()=>setActiveRoom(null)}>&times;</b>
           <ul className={'flex flex-col gap-4'}>
             {keys.map(key => key !== 'idx' && <li key={key} className={'flex w-full gap-4'}>
